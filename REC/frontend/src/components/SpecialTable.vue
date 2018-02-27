19bc3a913a17fb17d5f834f383b3b544a06c30e5
zz bct <1668856072@qq.com>
2018-02-27 17:15:35 +0800

    add rucm

---
+++ REC/frontend/src/components/SpecialTable.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="view">
+    <div class="title">
+      <div>{{ title }} </div>
+      <div class="btn" v-if="tag === 'RFS'">
+        <span  @click="addSpec">+新增</span>
+        <span @click="delSpec">-删除</span>
+      </div>
+    </div>
+    <div class="table">
+      <div class="tag">
+        <span>{{ tag }}</span>
+        <input v-if="tag === 'RFS'">
+      </div>
+      <Table :colums = "colums" :datas = "datas" addAble></Table>
+    </div>
+  </div>
+</template>
+<style scoped>
+  input {
+    outline: none;
+    border: none;
+  }
+  .view {
+    display: flex;
+    justify-content: space-between;
+  }
+  .title {
+    width: 23%;
+    padding: 0 10px;
+    background-color: #bcbcbc;
+  }
+  .table {
+    flex-grow: 1;
+  }
+  .tag {
+    font-weight: bolder;
+    color: blue;
+    border: 1px solid #ccc;
+  }
+  .btn {
+    display: flex;
+    justify-content: space-between;
+    color: blue;
+    cursor: pointer;
+  }
+</style>
+<script>
+  import Table from '@/components/Table'
+  export default{
+    data () {
+      return {
+      }
+    },
+    props: {
+      colums: {
+        type: Array,
+        default: []
+      },
+      datas: {
+        type: Array,
+        default: []
+      },
+      title: {
+        type: String,
+        default: ''
+      },
+      tag: {
+        type: String,
+        default: ''
+      },
+      pos: {
+        type: Number,
+        default: 0
+      }
+    },
+    components: {
+      Table
+    },
+    methods: {
+      addSpec () {
+        let obj = {
+          title: this.title,
+          pos: this.pos
+        }
+        this.$emit('add', obj)
+      },
+      delSpec () {
+        let obj = {
+          title: this.title,
+          pos: this.pos
+        }
+        this.$emit('del', obj)
+      }
+    }
+  }
+</script>
